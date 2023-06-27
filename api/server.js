f92543762825fc3d083f86d693796d0f6dfeca5b
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-06-27 21:28:01 +0300

    initial

--- api/server.js
+++ api/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const useRouter = require("./users/users-router");
 
 const server = express();
 
 // ekspres'in varsayılan olarak istek gövdelerinde JSON'u ayrıştıramayacağını unutmayın
+server.use(express.json());
 
 // global ara yazılımlar ve kullanıcı routelarının buraya bağlanması gerekir
-
+server.use("/api/users",useRouter);
 server.get('/', (req, res) => {
-  res.send(`<h2>Biraz ara yazılım yazalım!</h2>`);
+  res.send(`server running on ${port}`);
 });
 
 module.exports = server;
