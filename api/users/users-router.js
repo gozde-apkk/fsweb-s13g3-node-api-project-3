d4c104452cc0c4c7b2c36de5040ffb46c9d976da
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-06-27 22:53:15 +0300

    Middleware all finished

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -2,40 +2,54 @@ const express = require('express');
 
 // `users-model.js` ve `posts-model.js` sayfalarına ihtiyacınız var
 // ara yazılım fonksiyonları da gereklidir
+const userModel = require("./users-model");
+const postModel = require("../posts/posts-model");
 
+const mw = require("../middleware/middleware");
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // TÜM KULLANICILARI İÇEREN DİZİYİ DÖNDÜRÜN
+  try{
+    let allUser = await userModel.get();
+    res.json(allUser);
+  }catch(error){
+    res.status(500).json({message:"error"})
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id',mw.validateUserId ,(req, res) => {
   // USER NESNESİNİ DÖNDÜRÜN
   // user id yi getirmek için bir ara yazılım gereklidir
+  try{
+    res.json(req.currentUser)
+  }catch(error){
+    next(error);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/',mw.validateUserId , (req, res) => {
   // YENİ OLUŞTURULAN USER NESNESİNİ DÖNDÜRÜN
   // istek gövdesini doğrulamak için ara yazılım gereklidir.
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', mw.validateUserId ,(req, res) => {
   // YENİ GÜNCELLENEN USER NESNESİNİ DÖNDÜRÜN
   // user id yi doğrulayan ara yazılım gereklidir
   // ve istek gövdesini doğrulayan bir ara yazılım gereklidir.
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', mw.validateUserId ,(req, res) => {
   // SON SİLİNEN USER NESNESİ DÖNDÜRÜN
   // user id yi doğrulayan bir ara yazılım gereklidir.
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts',mw.validateUserId , (req, res) => {
   // USER POSTLARINI İÇEREN BİR DİZİ DÖNDÜRÜN
   // user id yi doğrulayan bir ara yazılım gereklidir.
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', mw.validateUserId ,(req, res) => {
   // YENİ OLUŞTURULAN KULLANICI NESNESİNİ DÖNDÜRÜN
   // user id yi doğrulayan bir ara yazılım gereklidir.
   // ve istek gövdesini doğrulayan bir ara yazılım gereklidir.
